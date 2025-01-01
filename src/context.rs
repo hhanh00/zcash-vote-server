3afb010f391757175c554ea2edb01fd775d3af29
Hanh <hanh425@gmail.com>
2025-01-01 19:58:45 +1000

    feat: save elections from data dir to db

--- src/context.rs
+++ src/context.rs
@@ -2,15 +2,17 @@ use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
 
 pub struct Context {
+    pub data_path: String,
     pub db_path: String,
     pub pool: Pool<SqliteConnectionManager>,
 }
 
 impl Context {
-    pub fn new(db_path: String) -> Self {
+    pub fn new(data_path: String, db_path: String) -> Self {
         let pool = Pool::new(SqliteConnectionManager::file(&db_path)).unwrap();
 
         Self {
+            data_path,
             db_path,
             pool,
         }
