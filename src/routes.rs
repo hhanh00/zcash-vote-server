89d7669a09e25561b78b4b82da3e8bd98fb850bf
hhanh00 <hanh425@gmail.com>
2025-01-05 10:32:15 +1000

    feat: return 500 on validation error (#12)

--- src/routes.rs
+++ src/routes.rs
@@ -1,9 +1,9 @@
 use anyhow::Error;
-use rocket::{serde::json::Json, State};
+use rocket::{http::Status, response::status::Custom, serde::json::Json, State};
 use rusqlite::params;
 use serde_json::Value;
 use zcash_vote::{
-    as_byte256, ballot::Ballot, db::{store_dnf, store_prop}, election::{Frontier, OrchardHash}, Election
+    as_byte256, ballot::Ballot, db::store_dnf, election::{Frontier, OrchardHash}, Election
 };
 
 use crate::{context::Context, db::{check_cmx_root, get_election, store_ballot}};
@@ -20,40 +20,42 @@ pub fn get_election_by_id(id: String, state: &State<Context>) -> Result<Json<Val
 }
 
 #[rocket::get("/election/<id>/ballot/height/<height>")]
-pub fn get_ballot_height(id: String, height: u32, state: &State<Context>) -> Result<Json<Value>, String> {
+pub fn get_ballot_height(id: String, height: u32, state: &State<Context>) -> Result<Json<Value>, Custom<String>> {
     (|| {
         let connection = state.pool.get()?;
         let (id_election, _) = get_election(&connection, &id)?;
         let ballot = crate::db::get_ballot_height(&connection, id_election, height)?;
         let ballot = serde_json::from_str::<Value>(&ballot)?;
         Ok::<_, Error>(Json(ballot))
-    })().map_err(|e| e.to_string())
+    })().map_err(|e| Custom(Status::InternalServerError, e.to_string()))
 }
 
 #[rocket::get("/election/<id>/num_ballots")]
-pub fn get_num_ballots(id: String, state: &State<Context>) -> Result<String, String> {
+pub fn get_num_ballots(id: String, state: &State<Context>) -> Result<String, Custom<String>> {
     (|| {
         let connection = state.pool.get()?;
         let (id_election, _) = get_election(&connection, &id)?;
         let n = crate::db::get_num_ballots(&connection, id_election)?;
         Ok::<_, Error>(n.to_string())
-    })().map_err(|e| e.to_string())
+    })().map_err(|e| Custom(Status::InternalServerError, e.to_string()))
 }
 
 #[rocket::post("/election/<id>/ballot", format = "json", data = "<ballot>")]
-pub fn post_ballot(id: String, ballot: Json<Ballot>, state: &State<Context>) -> Result<String, String> {
+pub fn post_ballot(id: String, ballot: Json<Ballot>, state: &State<Context>) -> Result<String, Custom<String>> {
     let res = || {
         println!("Ballot received");
         let pool = &state.pool;
         let mut connection = pool.get()?;
-        let transaction = connection.transaction()?;
-        let (id_election, election) = get_election(&transaction, &id)?;
+        let (id_election, election) = get_election(&connection, &id)?;
         let election = serde_json::from_str::<Election>(&election)?;
+        let data = zcash_vote::validate::validate_ballot(ballot.clone().into_inner(), election.signature_required)?;
+        println!("Validated");
 
-        if &ballot.data.anchors.nf != &election.nf.0 {
+        let transaction = connection.transaction()?;
+        if &data.anchors.nf != &election.nf.0 {
             anyhow::bail!("Incorrect nullifier root");
         }
-        check_cmx_root(&transaction, id_election, &ballot.data.anchors.cmx)?;
+        check_cmx_root(&transaction, id_election, &data.anchors.cmx)?;
         let height = transaction.query_row(
             "SELECT MAX(height) FROM cmx_frontiers WHERE election = ?1", [id_election], 
             |r| r.get::<_, u32>(0))?;
@@ -62,7 +64,7 @@ pub fn post_ballot(id: String, ballot: Json<Ballot>, state: &State<Context>) -> 
             params![id_election, height],
             |r| r.get::<_, String>(0))?;
         let mut cmx_frontier = serde_json::from_str::<Frontier>(&cmx_frontier)?;
-        for action in ballot.data.actions.iter() {
+        for action in data.actions.iter() {
             cmx_frontier.append(OrchardHash(as_byte256(&action.cmx)));
             store_dnf(&transaction, id_election, &action.nf)?;
         }
@@ -74,13 +76,11 @@ pub fn post_ballot(id: String, ballot: Json<Ballot>, state: &State<Context>) -> 
         let height = crate::db::get_num_ballots(&transaction, id_election)?;
         println!("{height}");
         store_ballot(&transaction, id_election, height + 1, &ballot, &cmx_root)?;
-        let sighash = hex::encode(ballot.data.sighash()?);
+        let sighash = hex::encode(data.sighash()?);
         println!("{id_election} {sighash}");
-        zcash_vote::validate::validate_ballot(ballot.into_inner(), election.signature_required)?;
-        println!("Validated");
         transaction.commit()?;
         println!("Commited");
         Ok::<_, Error>(sighash)
     };
-    res().map_err(|e| e.to_string())
+    res().map_err(|e| Custom(Status::InternalServerError, e.to_string()))
 }
