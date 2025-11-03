1f69d9358b6b090966cadb78333aabacdb1b92e8
Hanh <hanh425@gmail.com>
2025-11-03 16:21:44 +0800

    fix: missing create_if_missing

--- src/context.rs
+++ src/context.rs
@@ -9,7 +9,8 @@ pub struct Context {
 
 impl Context {
     pub async fn new(data_path: String, db_path: String, comet_bft: u16) -> Self {
-        let options = SqliteConnectOptions::new().filename(&db_path);
+        let options = SqliteConnectOptions::new().filename(&db_path)
+            .create_if_missing(true);
         let pool =
             SqlitePool::connect_with(options).await.unwrap();
 
