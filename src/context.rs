4fd2e113848c5a41c42dbea4d306d841d20e68cc
Hanh <hanh425@gmail.com>
2025-11-03 12:33:26 +0800

    feat: async mode

--- src/context.rs
+++ src/context.rs
@@ -1,16 +1,17 @@
-use r2d2::Pool;
-use r2d2_sqlite::SqliteConnectionManager;
+use sqlx::{sqlite::SqliteConnectOptions, SqlitePool};
 
 pub struct Context {
     pub data_path: String,
     pub db_path: String,
     pub comet_bft: u16,
-    pub pool: Pool<SqliteConnectionManager>,
+    pub pool: SqlitePool,
 }
 
 impl Context {
-    pub fn new(data_path: String, db_path: String, comet_bft: u16) -> Self {
-        let pool = Pool::new(SqliteConnectionManager::file(&db_path)).unwrap();
+    pub async fn new(data_path: String, db_path: String, comet_bft: u16) -> Self {
+        let options = SqliteConnectOptions::new().filename(&db_path);
+        let pool =
+            SqlitePool::connect_with(options).await.unwrap();
 
         Self {
             data_path,
